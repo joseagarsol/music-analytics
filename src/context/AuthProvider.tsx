454316a439d6ec746ee5cbd7c81fc4eb4b544166
José Antonio García Solano <joseagarsol@gmail.com>
2025-12-16 19:44:18 +0100

    feat: implementar manejo de tokens de acceso y refresco para autenticación de Spotify

--- src/context/AuthProvider.tsx
+++ src/context/AuthProvider.tsx
@@ -1,12 +1,28 @@
-import { useState, useMemo } from 'react';
+import { useState, useMemo, useEffect } from 'react';
 import { AuthContext } from './AuthContext';
 import { getStoredToken } from '../services/auth';
+import type { UserTokenResponse } from '../types/login';
 
 export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
   const [token, setToken] = useState<string | null>(getStoredToken());
 
-  const login = (token: string) => {
-    setToken(token);
+  useEffect(() => {
+    const handleTokenRefresh = (event: Event) => {
+      const customEvent = event as CustomEvent<string>;
+      setToken(customEvent.detail);
+    };
+
+    window.addEventListener('spotify_token_refreshed', handleTokenRefresh);
+
+    return () => {
+      window.removeEventListener('spotify_token_refreshed', handleTokenRefresh);
+    };
+  }, []);
+
+  const login = (user: UserTokenResponse) => {
+    window.localStorage.setItem('spotify_token', user.access_token);
+    window.localStorage.setItem('spotify_refresh_token', user.refresh_token);
+    setToken(user.access_token);
   };
 
   const logout = () => {
